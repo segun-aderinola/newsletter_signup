ad26dc68a3b880d079e0c0d1ca5b286684286d0e
segun-aderinola <segunjames554@gmail.com>
2022-06-20 05:21:17 +0100

    added Procfile

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.post("/signup", (req, res)=>{
     res.redirect("/")
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log("App is listening on "+port);
 })
